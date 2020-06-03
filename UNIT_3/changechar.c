4c060c8fa5096d6c52b5724dee06769dfe3e11b1
GabrielISB <61764384+GabrielISB@users.noreply.github.com>
2020-06-02 22:21:20 -0500

    Update changechar.c

--- UNIT_3/changechar.c
+++ UNIT_3/changechar.c
@@ -3,28 +3,28 @@
 
 void replace(char char_array[]) 
 {
-	int i;          /* index into an array */
+int i;          /* index into an array */
 
-	for (i = 0; i < sizeof(char_array); ++i) 
+for (i = 0; i < sizeof(char_array); ++i) 
+{
+	if (char_array[i] == '-')
 	{
-		if (char_array[i] == '-')
-		{
-			char_array[i] = '_';
-		}
+	char_array[i] = '_';
 	}
+}
 }
 
 int main(void) {
-	int i;          /* index into an array */
+int i;          /* index into an array */
 
-	char chars[] = { 'f', 'o', 'o', '-', 'b', '_', 'r' };
-	replace(chars);
+char chars[] = { 'f', 'o', 'o', '-', 'b', '_', 'r' };
+replace(chars);
 
-	for (i = 0; i < sizeof(chars); ++i) 
-	{
-		printf("%c\n", chars[i]);
-	}
+for (i = 0; i < sizeof(chars); ++i) 
+{
+printf("%c\n", chars[i]);
+}
 
-	return(0);
+return(0);
 }
 
