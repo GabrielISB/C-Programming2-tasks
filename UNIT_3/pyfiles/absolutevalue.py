305594821bab47b02d71315cef2f6a157c015969
GabrielISB <1909164@upy.edu.mx>
2020-07-16 17:08:03 -0500

    do not repeat yourself

---
+++ UNIT_3/pyfiles/absolutevalue.py
@@ -0,0 +1,21 @@
+def absolute_value(num):
+    """This function returns the absolute
+    value of the entered number"""
+
+    if num >= 0:
+        return num
+    else:
+        return -num
+
+
+print(absolute_value(99))
+
+print(absolute_value(-4))
+
+print(absolute_value(7884))
+
+print(absolute_value(4))
+
+print(absolute_value(-7899))
+
+print(absolute_value(-9))
---
+++ UNIT_3/pyfiles/dist2points.py
@@ -0,0 +1,9 @@
+#This is a short program where you can find the mid point between two points witouht repeting the code
+#Define the func
+def distance(x, y):
+	
+	return (x+y)/2
+##Then only call the function with its values
+print("The mid point is")
+print("X = ", distance(5, 9))
+print("Y = ", distance(-10, -7))
